a179486bd092aa37b5a19831337dfde7f8117201
Renato Maia <renato171179@gmail.com>
2023-05-13 21:22:09 -0400

    :feat atualizacao

---
+++ components/template/TemplateRecover.vue
@@ -0,0 +1,81 @@
+<template>
+  <div class="recoverPassword">
+    <div class="titleRecover">
+      <h2>Esqueceu a senha?</h2>
+      <img src="~/assets/icons/faceRecover.svg" alt="faceRecover" />
+    </div>
+    <h3>Preencha com seu e-mail!</h3>
+    <div class="inputRecover">
+      <input type="email" placeholder="Digite seu email" v-model="email" />
+      <div class="btnRecover">
+        <Button @click.native="backPage" title="Voltar" />
+        <Button @click.native="recoverEmail" title="Recuperar Senha" />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+import httpRecover from '@/server/auth'
+export default Vue.extend({
+  data() {
+    return {
+      email: '',
+    }
+  },
+  methods: {
+    backPage() {
+      this.$router.push('/login')
+    },
+    async recoverEmail() {
+      if (this.email) {
+        this.$toast.warning('Preencha todos os campos!!!')
+      }
+      await httpRecover
+        .PostRecoverEmail(this.email)
+        .then((res) => {
+          sessionStorage.setItem('token', res.data)
+          console.log(res)
+        })
+        .catch((error) => {
+          console.log(error)
+        })
+    },
+  },
+})
+</script>
+
+<style scoped lang="scss">
+.recoverPassword {
+  width: 100%;
+  height: 91vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  gap: 1rem;
+  .titleRecover {
+    display: flex;
+    gap: 1rem;
+  }
+  .inputRecover {
+    width: 80%;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    gap: 1.2rem;
+    input,
+    button {
+      width: 50%;
+    }
+    .btnRecover {
+      width: 50%;
+      display: flex;
+      gap: 2rem;
+    }
+  }
+}
+</style>
--- pages/recoverPassword.vue
+++ pages/recoverPassword.vue
@@ -1,73 +1,12 @@
 <template>
-  <div class="recoverPassword">
-    <div class="titleRecover">
-      <h2>Esqueceu a senha?</h2>
-      <img src="~/assets/icons/faceRecover.svg" alt="faceRecover" />
-    </div>
-    <h3>Preencha com seu e-mail!</h3>
-    <div class="inputRecover">
-      <input type="email" placeholder="Digite seu email" v-model="email" />
-      <div class="btnRecover">
-        <Button @click.native="backPage" title="Voltar" />
-        <Button @click.native="recoverEmail" title="Recuperar Senha" />
-      </div>
-    </div>
-  </div>
+  <TemplateRecover />
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
 
-import httpRecover from '@/server/auth'
-export default Vue.extend({
-  data() {
-    return {
-      email: '',
-    }
-  },
-  methods: {
-    backPage() {
-      this.$router.push('/login')
-    },
-    async recoverEmail() {
-      await httpRecover.PostRecoverEmail(this.email).then((res) => {
-        sessionStorage.setItem('token', res.data)
-        console.log(res)
-      })
-    },
-  },
-})
+export default Vue.extend({})
 </script>
 
-<style scoped lang="scss">
-.recoverPassword {
-  width: 100%;
-  height: 91vh;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  gap: 1rem;
-  .titleRecover {
-    display: flex;
-    gap: 1rem;
-  }
-  .inputRecover {
-    width: 80%;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: center;
-    gap: 1.2rem;
-    input,
-    button {
-      width: 50%;
-    }
-    .btnRecover {
-      width: 50%;
-      display: flex;
-      gap: 2rem;
-    }
-  }
-}
+<style scoped>
 </style>
