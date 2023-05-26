3fcf376aad4a14ea5695179dcd228074919c7f72
Renato Maia <renato171179@gmail.com>
2023-05-26 05:23:12 -0400

    fix create ingrediente estoque e modal estoque

---
+++ components/atoms/ContainerModalEdit.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="container_edit_modal" v-if="closeModal">
+    <div class="negativeSpace">
+      <slot />
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  props: {
+    closeModal: {
+      type: Boolean,
+      required: true,
+    },
+  },
+})
+</script>
+
+<style scoped lang="scss">
+.container_edit_modal {
+  width: 100%;
+  height: 100%;
+  position: fixed;
+  top: 0;
+  right: 0;
+  background-color: var(--bg_color_modal);
+  display: table;
+  transition: opacity 0.2s ease;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 2rem 0;
+  .negativeSpace {
+    width: 70%;
+    height: 80%;
+    display: flex;
+    flex-direction: column;
+    gap: 2rem;
+    background: var(--bg_color);
+    border-radius: 7px;
+  }
+}
+</style>
---
+++ components/organisms/Ingredientes/CreateIngredientes.vue
@@ -0,0 +1,60 @@
+<template>
+  <Container>
+    <Title>
+      <h1>Novo Ingrediente</h1>
+    </Title>
+    <div class="input_create">
+      <div class="input">
+        <Label>Nome</Label>
+        <input
+          type="text"
+          placeholder="Digite o nome ingrediente"
+          v-model="name_ingrediente"
+        />
+      </div>
+      <div class="input">
+        <Label>Preco</Label>
+        <input
+          type="number"
+          placeholder="Digite o preco ingrediente"
+          v-model="price_ingrediente"
+        />
+      </div>
+    </div>
+    <Button title="Salvar" />
+  </Container>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  data() {
+    return {
+      name_ingrediente: '',
+      price_ingrediente: '',
+    }
+  },
+})
+</script>
+
+<style scoped lang="scss">
+.input_create {
+  width: 100%;
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  gap: 1.5rem;
+  .input_column {
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    gap: 1rem;
+  }
+  .input {
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    gap: 0.3rem;
+  }
+}
+</style>
