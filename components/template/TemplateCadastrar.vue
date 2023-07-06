fbad47bd352cc695b12e7279a4a32302b6a4e049
William David Martins de Almeida <wdma.lic17@uea.edu.br>
2023-07-06 15:20:29 -0400

    update menubar width

--- components/template/TemplateCadastrar.vue
+++ components/template/TemplateCadastrar.vue
@@ -148,9 +148,10 @@ export default Vue.extend({
 
 <style lang="scss" scoped>
 .containerGrid {
-  display: grid;
-  grid-template: calc(4rem - 100vh) / 25rem 1fr;
+  display: flex;
+  
   .scroll_container {
+    width: 80%;
     height: 90vh;
     overflow-y: scroll;
     border-left: 1px solid var(--border);
