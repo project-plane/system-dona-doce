9acbbf3ed8c38a5adcedc9801bead3b45f132cc6
William David Martins de Almeida <wdma.lic17@uea.edu.br>
2023-09-21 16:51:03 -0400

    integracao kanban funcional

--- components/template/TemplateKanbanProduto.vue
+++ components/template/TemplateKanbanProduto.vue
@@ -27,7 +27,7 @@
                 </div>
 
                 <draggable v-model="list" class="kanban-list" ghost-class="ghost">
-                    <CardKanban v-for="(item, index) in list" :key="index" :data-object="item" :type-card="typeKanban"/>
+                    <CardKanban v-for="(item, index) in listLanche01" :key="index" :data-object="item" :type-card="typeKanban"/>             
                 </draggable>
             </div>
 
@@ -39,7 +39,7 @@
                 </div>
 
                 <div class="kanban-list">
-                   
+                    <CardKanban v-for="(item, index) in listLanche02" :key="index" :data-object="item" :type-card="typeKanban"/>
                 </div>
             </div>
 
@@ -49,10 +49,14 @@
                     <span v-else>15:00</span>
                     <span>Total: 1.102</span>
                 </div>
+
+                <div class="kanban-list">
+                    <CardKanban v-for="(item, index) in listDejejum" :key="index" :data-object="item" :type-card="typeKanban"/>
+                </div>
       
             </div>
 
-            {{ listKanban }}
+            
         </div>
     </div>
 </template>
@@ -76,13 +80,31 @@ export default Vue.extend({
                 {id: 1, nameRecipe: 'Cookie', qtde: 200, horario: '10:00', idOrdem: 0, cliente: 'Honda'},
                 {id: 2, nameRecipe: 'Jacaré', qtde: 500, horario: '10:00', idOrdem: 0, cliente: 'Honda'},
             ],
-            listKanban: []
+            listKanban: [],
+            listLanche01: [],
+            listLanche02: [],
+            listDejejum: []
         }
     },
 
     async created() {
         await httpKanban.GetKanban().then( (res) => {
             this.listKanban = res.data
+
+            res.data.map( (item) => {
+                if(item.description_category === 'Lanche 1') {
+                    this.listLanche01.push(item)
+                }
+
+                if(item.description_category === 'Lanche 2') {
+                    this.listLanche02.push(item)
+                }
+
+                if(item.description_category === 'Dejejum') {
+                    this.listDejejum.push(item)
+                }
+                
+            })
         })
     },
 
