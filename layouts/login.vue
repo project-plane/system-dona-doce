1a0d75d6e9d0209fd5ffc28a8dcfae898614d5bf
luan santos <luan.santos@grupoicts.com.br>
2023-10-20 13:51:00 -0400

    tela login

--- layouts/login.vue
+++ layouts/login.vue
@@ -16,13 +16,39 @@ export default Vue.extend({})
   width: 100%;
   height: 100vh;
   background-image: url('~/assets/img/bg_login.png');
-  background-size: cover;
+  animation: fadeIn 5s, slidein 3s;
+  // background-size: cover;
   opacity: 1;
+  overflow: hidden;
   background-position: bottom center;
+
   .mascara {
     width: 100%;
     height: 100vh;
     background: var(--bg_opacity);
+  }
+
+  @keyframes slidein {
+  from {
+    margin-left: 100%;
+    width: 0%;
+    overflow: hidden;
+  }
+
+  to {
+    margin-left: 0%;
+    width: 100%;
+    overflow: hidden;
+  }
+}
+
+  @keyframes fadeIn {
+    0% {
+      opacity: 0;
+    }
+    100% {
+      opacity: 1;
+    }
   }
 }
 </style>
