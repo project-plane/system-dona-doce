34cb93bc424fe28879226807326269a5034e483a
Maria Eduarda <Maria.Eduarda@tutiplast.local>
2023-10-25 01:29:07 -0400

    correcao do loop do metodo valueorder e modal historico

--- components/molecules/CardHistorico.vue
+++ components/molecules/CardHistorico.vue
@@ -55,7 +55,10 @@ import Vue from 'vue'
 import dayjs from 'dayjs'
 export default Vue.extend({
   props: {
-    data: Object,
+    data: {
+      type: [Array, Object],
+      required: true,
+    },
   },
 
   data() {
@@ -68,18 +71,22 @@ export default Vue.extend({
 
   methods: {
     valueorder(listOrder) {
-      this.totalOrderValue = 0
-      listOrder.map((item) => {
-        this.totalOrderValue =
-          this.totalOrderValue +
-          Number(item.amountItem) * Number(item.valueOrderItem)
-      })
+    const totalOrderValue = listOrder.reduce((total, item) => {
+      const amount = Number(item.amountItem);
+      const value = Number(item.valueOrderItem);
+      const itemTotal = amount * value;
+      return total + itemTotal;
+    }, 0);
 
-      return this.totalOrderValue.toLocaleString('pt-BR', {
-        style: 'currency',
-        currency: 'BRL',
-      })
-    },
+    // Formatando o valor total como moeda BRL
+    const formattedTotal = totalOrderValue.toLocaleString('pt-BR', {
+      style: 'currency',
+      currency: 'BRL',
+    });
+
+    return formattedTotal;
+},
+
 
     formatDate(date) {
       return dayjs(date).format('DD/MM/YYYY')
