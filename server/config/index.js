79af924be481e80f548efd1a4e68593da53a7665
Eduarda Lima <me.eduuarda@gmail.com>
2023-12-26 14:07:35 -0400

    update branch

--- server/config/index.js
+++ server/config/index.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 const http = axios.create({
 
   // baseURL: 'http://localhost:3100',
-  baseURL: 'https://api.donadoce.gedroid.com',
+  baseURL: 'https://api.donadoce.net',
 
   headers: {
     Accept: 'application/json',
